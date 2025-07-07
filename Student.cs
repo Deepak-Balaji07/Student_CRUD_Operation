7e1d902f9115877d968e507df64895e1275a9865
Deepak Balaji <147257487+Deepak-Balaji07@users.noreply.github.com>
2025-07-07 22:21:04 +0530

    Add files via upload

---
+++ Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public Student(int Id , string Name , string Department) { 
+            this.Id = Id;
+            this.Name = Name;
+            this.Department = Department;
+        }
+
+    }
+}
---
+++ StudentController.cs
@@ -0,0 +1,19 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    public class StudentController
+    {
+        string connectionString = " Server = localhost; Database = student_table ; id = root ; pwd = 1234 ; ";
+        MySqlConnection conn;
+    }
+    public void GetStudent(int id)
+    {
+          
+    }
+}
